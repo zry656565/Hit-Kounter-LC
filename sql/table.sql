05c0099af1ef98ed6df51a618f9ad7f8b975695c
Jerry Zou <zry656565@gmail.com>
2015-06-23 23:52:58 +0800

    remove field `UA_Type`

--- sql/table.sql
+++ sql/table.sql
@@ -27,7 +27,6 @@ CREATE TABLE `page` (
   `domain` varchar(100) NOT NULL,
   `url` varchar(300) NOT NULL,
   `count` int(11) NOT NULL DEFAULT '0',
-  `UA_type` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
