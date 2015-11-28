b3f1a19690b80163a9b6064f9824a63042dff898
Jerry Zou <jerry.zry@outlook.com>
2015-11-28 20:44:17 +0800

    fix those wrong test cases

--- test/page.js
+++ test/page.js
@@ -30,107 +30,105 @@ $.jsonp(pageUrl, {
 })
 
 function f1(result) {
-    $.log(testcase, result.url, 'count: ' + result.count)
+    $.log(++testcase, result.url, 'count: ' + result.count)
     if (result.count != 1) {
         $.alert('Test for Increment fail: ' + result.url, 'count: ' + result.count)
     }
-    tryComplete()
+
+    /*****
+     * Test 1.1 increment
+     */
+    $.jsonp(pageUrl, {
+        callback: 'f11',
+        type: 'increment',
+        domain: 'httpsss://demo.com',
+        url: 'httpsss://demo.com/123/',
+        title: 'lalala'
+    })
 }
 
-/*****
- * Test 1.1 increment
- */
-$.jsonp(pageUrl, {
-    callback: 'f2',
-    type: 'increment',
-    domain: 'httpsss://demo.com',
-    url: 'httpsss://demo.com/123/',
-    title: 'lalala'
-})
-
-function f2(result) {
-    $.log(testcase, result.url, 'count: ' + result.count)
+function f11(result) {
+    $.log(++testcase, result.url, 'count: ' + result.count)
     if (result.count != 2) {
         $.alert('Test for Increment fail: ' + result.url, 'count: ' + result.count)
     }
-    tryComplete()
+
+    /*****
+     * Test 1.2 increment
+     */
+    $.jsonp(pageUrl, {
+        type: 'increment',
+        callback: 'f12',
+        domain: 'httpsss://demo.com',
+        url: 'httpsss://demo.com/123456/',
+        title: 'lalala'
+    })
 }
 
-/*****
- * Test 2.0 get
- */
-$.jsonp(pageUrl, {
-    type: 'increment',
-    callback: 'nullFunc',
-    domain: 'httpsss://demo.com',
-    url: 'httpsss://demo.com/123456/',
-    title: 'lalala'
-})
+function f12(result) {
+    /*****
+     * Test 2.0 get
+     */
+    $.jsonp(pageUrl, {
+        callback: 'f2',
+        type: 'get',
+        pages: JSON.stringify([
+            { domain: "httpsss://demo.com", url: "httpsss://demo.com/123456/" },
+            { domain: "httpsss://demo.com", url: "httpsss://demo.com/123/" }
+        ])
+    });
+}
 
-$.jsonp(pageUrl, {
-    callback: 'f3',
-    type: 'get',
-    pages: JSON.stringify([
-        { domain: "httpsss://demo.com", url: "httpsss://demo.com/123456/" },
-        { domain: "httpsss://demo.com", url: "httpsss://demo.com/123/" }
-    ])
-});
-
-function f3(results) {
-    $.log(testcase, 'test for get')
+function f2(results) {
+    $.log(++testcase, 'test for getTop')
     if (results[0].count != 1) $.alert('Test for Get fail')
     if (results[1].count != 2) $.alert('Test for Get fail')
-    tryComplete()
+
+    /*****
+     * Test 3.0 getTop
+     */
+
+    $.jsonp(pageUrl, {
+        callback: 'f3',
+        type: 'getTop',
+        domain: 'httpsss://demo.com',
+        number: '1'
+    });
 }
 
-/*****
- * Test 3.0 getTop
- */
-
-$.jsonp(pageUrl, {
-    callback: 'f4',
-    type: 'getTop',
-    domain: 'httpsss://demo.com',
-    number: '1'
-});
-
-function f4(results) {
-    $.log(testcase, 'test for get top 1')
+function f3(results) {
+    $.log(++testcase, 'test for getTop 1')
     if (results[0].count != 2) $.alert('Test for getTop fail')
-    tryComplete()
+
+    /*****
+     * Test 3.1 getTop
+     */
+
+    $.jsonp(pageUrl, {
+        callback: 'f31',
+        type: 'getTop',
+        domain: 'httpsss://demo.com',
+        number: '2'
+    });
 }
 
-/*****
- * Test 3.1 getTop
- */
-
-$.jsonp(pageUrl, {
-    callback: 'f5',
-    type: 'getTop',
-    domain: 'httpsss://demo.com',
-    number: '2'
-});
-
-function f5(results) {
-    $.log(testcase, 'test for get top 2')
+function f31(results) {
+    $.log(++testcase, 'test for getTop 2')
     if (results[0].count != 2) $.alert('Test for getTop fail')
     if (results[1].count != 1) $.alert('Test for getTop fail')
-    tryComplete()
+
+    /*****
+     * Test 4.0 getByDomain
+     */
+    $.jsonp(pageUrl, {
+        callback: 'f4',
+        type: 'getByDomain',
+        domain: 'httpsss://demo.com'
+    });
 }
 
-
-/*****
- * Test 4.0 getByDomain
- */
-
-$.jsonp(pageUrl, {
-    callback: 'f6',
-    type: 'getByDomain',
-    domain: 'httpsss://demo.com'
-});
-
-function f6(results) {
-    $.log(testcase, 'test for get by domain')
+function f4(results) {
+    $.log(++testcase, 'test for get by domain')
     if (results[0].count != 2) $.alert('Test for getByDomain fail')
     if (results[1].count != 1) $.alert('Test for getByDomain fail')
     tryComplete()
