983388f9128f14e93ef33da9ab45412287126656
Jerry Zou <jerry.zry@outlook.com>
2016-11-03 15:59:04 +0800

    run the job in a fixed time interval

--- jobs/calc-site-views.js
+++ jobs/calc-site-views.js
@@ -3,6 +3,7 @@
 const AV = require('leancloud-storage')
 const APP_ID = 'yzbpXQpXf1rWVRfAAM8Durgh-gzGzoHsz'
 const APP_KEY = '020bjTvbiVinVQ21YtWAJ9t8'
+const INTERVAL = 10 * 60 * 1000;
 
 AV.init({
   appId: APP_ID,
@@ -10,28 +11,33 @@ AV.init({
 })
 
 const Site = AV.Object.extend('Site')
-const counter = {}
 
-let query = new AV.Query('Page')
-query.each((page) => {
-  let d = page.get('domain')
-  if (counter[d]) counter[d] += page.get('count')
-  else counter[d] = page.get('count')
-}).then(() => {
-  console.log('Collect all pages.')
-  for (let domain in counter) {
-    if (counter.hasOwnProperty(domain)) {
-      let siteView = counter[domain]
-      let siteQ = new AV.Query('Site')
-      siteQ.equalTo('domain', domain)
-      siteQ.first().then(site => {
-        if (!site) {
-          site = new Site()
-          site.set('domain', domain)
-        }
-        site.set('count', siteView).save()
-        console.log(site.get('domain'), site.get('count'))
-      })
+setInterval(calc, INTERVAL);
+
+function calc() {
+  let counter = {}
+
+  let query = new AV.Query('Page')
+  query.each((page) => {
+    let d = page.get('domain')
+    if (counter[d]) counter[d] += page.get('count')
+    else counter[d] = page.get('count')
+  }).then(() => {
+    console.log('Collect all pages.')
+    for (let domain in counter) {
+      if (counter.hasOwnProperty(domain)) {
+        let siteView = counter[domain]
+        let siteQ = new AV.Query('Site')
+        siteQ.equalTo('domain', domain)
+        siteQ.first().then(site => {
+          if (!site) {
+            site = new Site()
+            site.set('domain', domain)
+          }
+          site.set('count', siteView).save()
+        })
+      }
     }
-  }
-})
+    console.log('Done.');
+  })
+}
